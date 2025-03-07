b3d5c32597736a6e32c4062c93cd6e195d382abe
Trevor Tu <toantu3005@gmail.com>
2025-03-07 15:47:40 +1100

    removed hard-coded path in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3001";
+const API_BASE_URL = process.env.API_BASE_URL;
 
 const nextConfig: NextConfig = {
   /* config options here */
